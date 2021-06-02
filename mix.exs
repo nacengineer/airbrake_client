090fa1589dd37010c38c25517a60ebf6fb04a66b
Jeremy D. Frens <jfrens@thecitybase.com>
2021-06-02 15:08:50 -0500

    issue-8 bump patch version to v0.8.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Airbrake.Mixfile do
   def project do
     [
       app: :airbrake,
-      version: "0.8.0",
+      version: "0.8.1",
       elixir: "~> 1.7",
       package: package(),
       description: """
