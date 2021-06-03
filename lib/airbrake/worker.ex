romul/airbrake-elixir

Use HTTPoison as default http_adapter
Configs from a library are not normally imported by a project
that uses that library. This sets HTTPoisong as a default HTTP
handler if/when the config for a handler is not present.

lib/airbrake/worker.ex
@@ -10,7 +10,9 @@ defmodule Airbrake.Worker do
   @name __MODULE__
   @request_headers [{"Content-Type", "application/json"}]
   @default_host "https://airbrake.io"
-  @http_adapter Application.get_env(:airbrake, :http_adapter)
+  @http_adapter :airbrake
+                |> Application.get_env(:private, [])
+                |> Keyword.get(:http_adapter, HTTPoison)
 
   @doc """
   Send a report to Airbrake.

nacengineer/airbrake_client

lib/airbrake/worker.ex
@@ -10,7 +10,9 @@ defmodule Airbrake.Worker do
   @name __MODULE__
   @request_headers [{"Content-Type", "application/json"}]
   @default_host "https://airbrake.io"
-  @http_adapter Application.get_env(:airbrake_client, :http_adapter)
+  @http_adapter :airbrake_client
+                |> Application.get_env(:private, [])
+                |> Keyword.get(:http_adapter, HTTPoison)
 
   @doc """
   Send a report to Airbrake.