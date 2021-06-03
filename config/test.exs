romul/airbrake-elixir

Set http adapter in environment-specific configs

config/test.exs
@@ -1,0 +1,4 @@
+use Mix.Config
+
+config :airbrake,
+  http_adapter: Airbrake.HTTPMock

nacengineer/airbrake_client

config/test.exs
@@ -1,0 +1,4 @@
+use Mix.Config
+
+config :airbrake_client,
+  http_adapter: Airbrake.HTTPMock