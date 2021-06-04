98af898bae1845e00efbc9fd4884939e5d04e044
Jeremy D. Frens <jfrens@thecitybase.com>
2021-06-04 17:00:46 -0500

    issue-13 use static data for testing in PayloadTest

--- test/airbrake/payload_test.exs
+++ test/airbrake/payload_test.exs
@@ -2,29 +2,24 @@ defmodule Airbrake.PayloadTest do
   use ExUnit.Case
   alias Airbrake.Payload
 
-  def get_problem do
-    try do
-      # If the following line is not on line 9 then tests will start failing.
-      # You've been warned!
-      apply(Harbour, :cats, [3])
-    rescue
-      exception -> [exception, __STACKTRACE__]
-    end
-  end
+  @exception %UndefinedFunctionError{
+    arity: 1,
+    function: :cats,
+    message: nil,
+    module: Harbour,
+    reason: nil
+  }
 
-  def get_payload(options \\ []) do
-    apply(Payload, :new, List.insert_at(get_problem(), -1, options))
-  end
-
-  def get_error(options \\ []) do
-    %{errors: [error]} = get_payload(options)
-    error
-  end
-
-  def get_context(options \\ []) do
-    %{context: context} = get_payload(options)
-    context
-  end
+  @stacktrace [
+    {Harbour, :cats, [3], []},
+    {:erl_eval, :do_apply, 6, [file: 'erl_eval.erl', line: 680]},
+    {:erl_eval, :try_clauses, 8, [file: 'erl_eval.erl', line: 914]},
+    {:elixir, :recur_eval, 3, [file: 'src/elixir.erl', line: 280]},
+    {:elixir, :eval_forms, 3, [file: 'src/elixir.erl', line: 265]},
+    {IEx.Evaluator, :handle_eval, 5, [file: 'lib/iex/evaluator.ex', line: 261]},
+    {IEx.Evaluator, :do_eval, 3, [file: 'lib/iex/evaluator.ex', line: 242]},
+    {IEx.Evaluator, :eval, 3, [file: 'lib/iex/evaluator.ex', line: 220]}
+  ]
 
   describe "new/2 and new/3" do
     test "generates report with a REAL exception" do
@@ -71,11 +66,15 @@ defmodule Airbrake.PayloadTest do
     end
 
     test "it reports the error class" do
-      assert "UndefinedFunctionError" == get_error().type
+      assert %{errors: [error]} = Payload.new(@exception, @stacktrace)
+      assert "UndefinedFunctionError" == error.type
     end
 
     test "it reports the error message" do
-      assert "function Harbour.cats/1 is undefined (module Harbour is not available)" == get_error().message
+      assert %{errors: [error]} = Payload.new(@exception, @stacktrace)
+
+      assert "function Harbour.cats/1 is undefined (module Harbour is not available)" ==
+               error.message
     end
 
     test "it generates correct stacktraces" do
@@ -100,15 +99,19 @@ defmodule Airbrake.PayloadTest do
     end
 
     test "it generates correct stacktraces when the current file was a script" do
+      assert %Payload{errors: [error]} = Payload.new(@exception, @stacktrace)
+
+      # This is TEMPORARY.  The stacktrace to backtrace translation needs better tests.
       assert [
-               %{file: "unknown", line: 0, function: _},
-               %{
-                 file: "test/airbrake/payload_test.exs",
-                 line: 9,
-                 function: "Elixir.Airbrake.PayloadTest.get_problem/0"
-               },
-               %{file: "test/airbrake/payload_test.exs", line: _, function: _} | _
-             ] = get_error().backtrace
+               %{file: "unknown", function: "Elixir.Harbour.cats(3)", line: 0},
+               %{file: "erl_eval.erl", function: "erl_eval.do_apply/6", line: 680},
+               %{file: "erl_eval.erl", function: "erl_eval.try_clauses/8", line: 914},
+               %{file: "src/elixir.erl", function: "elixir.recur_eval/3", line: 280},
+               %{file: "src/elixir.erl", function: "elixir.eval_forms/3", line: 265},
+               %{file: "lib/iex/evaluator.ex", function: "Elixir.IEx.Evaluator.handle_eval/5", line: 261},
+               %{file: "lib/iex/evaluator.ex", function: "Elixir.IEx.Evaluator.do_eval/3", line: 242},
+               %{file: "lib/iex/evaluator.ex", function: "Elixir.IEx.Evaluator.eval/3", line: 220}
+             ] = error.backtrace
     end
 
     # NOTE: Regression test
@@ -130,17 +133,17 @@ defmodule Airbrake.PayloadTest do
 
     test "it reports the notifier" do
       assert %{name: "Airbrake Client", url: "https://github.com/CityBaseInc/airbrake_client", version: _} =
-               get_payload().notifier
+               Payload.new(@exception, @stacktrace).notifier
     end
 
     test "it adds the context when given" do
-      %{context: context} = get_payload(context: %{msg: "Potato#cake"})
+      %{context: context} = Payload.new(@exception, @stacktrace, context: %{msg: "Potato#cake"})
       assert "Potato#cake" == context.msg
     end
 
     test "it filters sensitive params" do
       Application.put_env(:airbrake_client, :filter_parameters, ["password"])
-      payload = get_payload(params: %{"password" => "top_secret", "x" => "y"})
+      payload = Payload.new(@exception, @stacktrace, params: %{"password" => "top_secret", "x" => "y"})
       assert "[FILTERED]" == payload.params["password"]
       assert "y" == payload.params["x"]
       Application.delete_env(:airbrake_client, :filter_parameters)
