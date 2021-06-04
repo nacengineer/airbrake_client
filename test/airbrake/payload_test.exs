0ba1dc0ea72d1f9a10136f30be20dc408cfa62ba
Jeremy D. Frens <jfrens@thecitybase.com>
2021-06-04 16:34:29 -0500

    issue-13 add describe block to PayloadTest

--- test/airbrake/payload_test.exs
+++ test/airbrake/payload_test.exs
@@ -26,75 +26,81 @@ defmodule Airbrake.PayloadTest do
     context
   end
 
-  test "it adds the context when given" do
-    %{context: context} = get_payload(context: %{msg: "Potato#cake"})
-    assert "Potato#cake" == context.msg
-  end
+  describe "new/2 and new/3" do
+    test "it adds the context when given" do
+      %{context: context} = get_payload(context: %{msg: "Potato#cake"})
+      assert "Potato#cake" == context.msg
+    end
 
-  test "it generates correct stacktraces" do
-    {exception, stacktrace} =
-      try do
-        Enum.join(3, 'million')
-      rescue
-        exception -> {exception, __STACKTRACE__}
-      end
+    test "it generates correct stacktraces" do
+      {exception, stacktrace} =
+        try do
+          Enum.join(3, 'million')
+        rescue
+          exception -> {exception, __STACKTRACE__}
+        end
 
-    %{errors: [%{backtrace: stacktrace}]} = Payload.new(exception, stacktrace, [])
+      %{errors: [%{backtrace: stacktrace}]} = Payload.new(exception, stacktrace, [])
 
-    assert [
-             %{file: "lib/enum.ex", line: _, function: _},
-             %{
-               file: "test/airbrake/payload_test.exs",
-               line: _,
-               function: "Elixir.Airbrake.PayloadTest.test it generates correct stacktraces/1"
-             }
-             | _
-           ] = stacktrace
-  end
+      assert [
+               %{file: "lib/enum.ex", line: _, function: _},
+               %{
+                 file: "test/airbrake/payload_test.exs",
+                 line: _,
+                 function: "Elixir.Airbrake.PayloadTest.test new/2 and new/3 it generates correct stacktraces/1"
+               }
+               | _
+             ] = stacktrace
+    end
 
-  test "it generates correct stacktraces when the current file was a script" do
-    assert [
-             %{file: "unknown", line: 0, function: _},
-             %{file: "test/airbrake/payload_test.exs", line: 9, function: "Elixir.Airbrake.PayloadTest.get_problem/0"},
-             %{file: "test/airbrake/payload_test.exs", line: _, function: _} | _
-           ] = get_error().backtrace
-  end
+    test "it generates correct stacktraces when the current file was a script" do
+      assert [
+               %{file: "unknown", line: 0, function: _},
+               %{
+                 file: "test/airbrake/payload_test.exs",
+                 line: 9,
+                 function: "Elixir.Airbrake.PayloadTest.get_problem/0"
+               },
+               %{file: "test/airbrake/payload_test.exs", line: _, function: _} | _
+             ] = get_error().backtrace
+    end
 
-  # NOTE: Regression test
-  test "it generates correct stacktraces when the method arguments are in place of arity" do
-    {exception, stacktrace} =
-      try do
-        apply(Foo, :bar, [:qux, 1, "foo\n"])
-      rescue
-        exception -> {exception, __STACKTRACE__}
-      end
+    # NOTE: Regression test
+    test "it generates correct stacktraces when the method arguments are in place of arity" do
+      {exception, stacktrace} =
+        try do
+          apply(Foo, :bar, [:qux, 1, "foo\n"])
+        rescue
+          exception -> {exception, __STACKTRACE__}
+        end
 
-    %{errors: [%{backtrace: stacktrace}]} = Payload.new(exception, stacktrace, [])
+      %{errors: [%{backtrace: stacktrace}]} = Payload.new(exception, stacktrace, [])
 
-    assert [
-             %{file: "unknown", line: 0, function: "Elixir.Foo.bar(:qux, 1, \"foo\\n\")"},
-             %{file: "test/airbrake/payload_test.exs", line: _, function: _} | _
-           ] = stacktrace
-  end
+      assert [
+               %{file: "unknown", line: 0, function: "Elixir.Foo.bar(:qux, 1, \"foo\\n\")"},
+               %{file: "test/airbrake/payload_test.exs", line: _, function: _} | _
+             ] = stacktrace
+    end
 
-  test "it reports the error class" do
-    assert "UndefinedFunctionError" == get_error().type
-  end
+    test "it reports the error class" do
+      assert "UndefinedFunctionError" == get_error().type
+    end
 
-  test "it reports the error message" do
-    assert "function Harbour.cats/1 is undefined (module Harbour is not available)" == get_error().message
-  end
+    test "it reports the error message" do
+      assert "function Harbour.cats/1 is undefined (module Harbour is not available)" == get_error().message
+    end
 
-  test "it reports the notifier" do
-    assert %{name: "Airbrake Client", url: "https://github.com/CityBaseInc/airbrake_client", version: _} =
-             get_payload().notifier
-  end
+    test "it reports the notifier" do
+      assert %{name: "Airbrake Client", url: "https://github.com/CityBaseInc/airbrake_client", version: _} =
+               get_payload().notifier
+    end
 
-  test "it filters sensitive params" do
-    Application.put_env(:airbrake_client, :filter_parameters, ["password"])
-    payload = get_payload(params: %{"password" => "top_secret", "x" => "y"})
-    assert "[FILTERED]" == payload.params["password"]
-    assert "y" == payload.params["x"]
-    Application.delete_env(:airbrake_client, :filter_parameters)
+    test "it filters sensitive params" do
+      Application.put_env(:airbrake_client, :filter_parameters, ["password"])
+      payload = get_payload(params: %{"password" => "top_secret", "x" => "y"})
+      assert "[FILTERED]" == payload.params["password"]
+      assert "y" == payload.params["x"]
+      Application.delete_env(:airbrake_client, :filter_parameters)
+    end
   end
 end
